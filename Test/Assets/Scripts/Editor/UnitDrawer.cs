341f51a8a3cb7702d1c65855bf12302562bb9425
Denis Sivtsov <dsivtsov@yahoo.com>
2020-12-14 06:25:13 +0300

    UnitDrawerTime

---
+++ Test/Assets/Scripts/Editor/UnitDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace BitStrap
+{
+    [CustomPropertyDrawer(typeof(UnitAttribute))]
+    public class UnitDrawer : PropertyDrawer
+    {
+        float widthGUIStyle,widthFromEditorStyle;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            UnitAttribute labelAttribute = attribute as UnitAttribute;
+            EditorGUI.PropertyField(position, property, label);
+            
+            if (Event.current.type != EventType.Repaint)
+            {
+                //((GUIStyle)"Label").CalcMinMaxWidth(label, out widthGUIStyle, out _ );
+                widthFromEditorStyle = EditorStyles.label.CalcSize(label).x;
+                //Debug.Log($"Event:{Event.current.type}: {widthFromEditorStyle} vs {widthGUIStyle}");
+            }
+            //((GUIStyle)"Label").CalcMinMaxWidth(label, out widthGUIStyle, out _);
+            //Debug.Log($"Event:{Event.current.type}: {EditorStyles.label.CalcSize(label).x} vs {widthGUIStyle}");
+            position.x = position.x + widthFromEditorStyle + 0f;
+            GUI.Label(position, labelAttribute.label, labelAttribute.labelStyle);
+        }
+    }
+}
---
+++ Test/Assets/Scripts/TestPlayerStatsClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TestPlayerStatsClass : MonoBehaviour
+{
+    [Unit("m/s²")]
+    public float EarthGravity = 9.80665f;
+
+
+
+    //Make the private field of our PlayerStats struct visible in the Inspector
+    //by applying [SerializeField] attribute to it
+    //[SerializeField]
+    //private PlayerStats[] FirstPlayerStats = new PlayerStats[]
+    //{
+    //    new PlayerStats { namePlayer = "Alfa", movementSpeed = 1, hitPoints = 1, hasHealthPotion = true },
+    //    new PlayerStats { namePlayer = "Beta", movementSpeed = 10, hitPoints = 5, hasHealthPotion = false },
+    //    new PlayerStats { namePlayer = "Gamma", movementSpeed = 100, hitPoints = 15, hasHealthPotion = true }
+    //};
+
+}
